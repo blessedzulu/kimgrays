a80992d86540148ab12748720e0c45d12ee4743c
Blessed Zulu <43051370+blessedzulu@users.noreply.github.com>
2021-10-15 18:39:26 +0200

    Minor changes and fixes

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,11 @@
-const me = {
-  name: "Blessed",
-  hobby: "Coding",
-};
+// fixme: Large gap between hero and about section on mobile
+// fixme: Spacing between sections feels small on mobile
+
+// todo: Tomorrow todo list
+// task: 1. Add site animations
+// task: 2. Add loading screen
+// task: 3. Add small screen nav menu
+// ?
+// *
+// dev:
+// !
